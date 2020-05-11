29988c36021a3f6e1f40629d31cf04e7a36c96e1
Pavlo Mitich <mitich.pavel@gmail.com>
2020-05-11 22:27:24 +0200

    remove nerds

--- nerds/dist/js/catalog_script.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-	//Modal
-	$('[data-modal=feedback]').on('click', function() {
-		$('.overlay, #feedback').fadeIn('slow');
-	});
-	$('.modal__close').on('click', function() {
-		$('.overlay, #feedback').fadeOut('slow');
-	});
-});
